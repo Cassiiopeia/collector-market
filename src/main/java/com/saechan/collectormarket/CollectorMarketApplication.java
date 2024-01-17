27de468312e2391a9263c1699f8a9633364a001a
서새찬 <chan4760@gmail.com>
2024-01-17 22:59:25 +0900

    init : [001-INITIAL] : Initial Commit

---
+++ src/main/java/com/saechan/collectormarket/CollectorMarketApplication.java
@@ -0,0 +1,13 @@
+package com.saechan.collectormarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saechan/collectormarket/CollectorMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saechan.collectormarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
